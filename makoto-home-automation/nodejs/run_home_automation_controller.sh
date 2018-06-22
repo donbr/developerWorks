467bf3cdfc9fee7293d5fb1240b4e805d302bf08
don-ibm <don.branson@ibm.com>
2018-06-22 15:52:20 -0700

    simplify repository

--- makoto-home-automation/nodejs/run_home_automation_controller.sh
+++
@@ -1,9 +0,0 @@
-#!
-
-# Make sure node has everything it needs
-echo "Running npm install to make sure Node.js is up-to-date..."
-npm install
-
-echo "Starting the Home Automation Controller..."
-# Run the home automation controller
-node homeAutomationController.js
